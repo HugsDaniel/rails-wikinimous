8df664f6e0c2297b7e9d93ac0ffc569c2ffb078d
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-07-11 15:52:34 +0200

    added user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
   get 'pages/home'
 
   get 'pages/about'
@@ -18,4 +22,17 @@ Rails.application.routes.draw do
   patch 'articles/:id', to: 'articles#update'
 
   delete "articles/:id", to: "articles#destroy"
+
+  # This route sends requests to our naked url to the *cool* action in the *gif* controller.
+  root to: 'gif#cool'
+
+  # I've created a gif controller so I have a page I can secure later.
+  # This is optional (as is the root to: above).
+  get '/cool' => 'gif#cool'
+  get '/sweet' => 'gif#sweet'
+
+  # These routes will be for signup. The first renders a form in the browse, the second will
+  # receive the form and create a user in our database using the data given to us by the user.
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 end
